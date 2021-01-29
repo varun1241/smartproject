fee1cc134d9edcc4471c8d9099ac28b7af813bee
varun1241 <varun124150@gmail.com>
2021-01-29 14:17:05 +0530

    first comit

--- school2/accounts/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Studentfrom,Employee
-
-admin.site.register(Studentfrom)
-
-admin.site.register(Employee)
-# Register your models here.
--- school2/accounts/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Studentfrom
-
-class StudentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model=Studentfrom
-        fields= '__all__'
